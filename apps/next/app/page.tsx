0ccfce5994aa1b4a4fefa818ef398d02aa3be4ff
Dmitrii Kopenkin s-dkopenkin@digikare.com <s-dkopenkin@digikare.com>
2024-02-04 13:49:07 +0100

    add example of communication between next & nest

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -5,7 +5,15 @@ const RootPage = () => {
   return (
     <div className="mx-auto mt-4 grid h-32 w-1/2 grid-rows-2 place-items-center gap-2 bg-glow-conic">
       <h1 className="text-3xl font-bold text-white">Boilerplate</h1>
-      <Button onClick={() => alert('Hello world!')}>Hi</Button>
+      <Button
+        onClick={() => {
+          fetch('http://localhost:3001/')
+            .then((res) => res.text())
+            .then((data) => alert(data))
+            .catch((err) => alert(err.message));
+        }}>
+        Hi
+      </Button>
     </div>
   );
 };
